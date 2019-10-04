aed1fa510c017ffd546a543e8c3bfc950a38c908
vamsi <vamsikrishna3415@outlook.in>
2019-10-04 19:10:48 +0530

    formatted code

--- saas-201-vamsikrishna-bellam/level-06/college_portal/app/controllers/departments_controller.rb
+++ saas-201-vamsikrishna-bellam/level-06/college_portal/app/controllers/departments_controller.rb
@@ -1,23 +1,19 @@
 class DepartmentsController < ApplicationController
-	def index
+  def index
     #Department.update_all('name = upper(name)')
-		@departments = Department.all
-
-	end
-
+    @departments = Department.all
+  end
 
   def new
     @department = Department.new
   end
 
   def create
-
     @department = Department.new(department_params)
 
     if @department.save
-      Department.update_all('name = upper(name)')
+      Department.update_all("name = upper(name)")
       redirect_to action: "index"
-
     else
       render "new"
     end
@@ -28,5 +24,4 @@ class DepartmentsController < ApplicationController
   def department_params
     params[:department].permit(:name)
   end
-  
 end
